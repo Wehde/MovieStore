538371aa6f3ba24d49214f98e820e5026fade11c
Wehde <kilianwehde@outlook.com>
2022-11-27 19:24:04 -0600

    Added Upcoming Releases

--- src/main/java/dmacc/repository/MovieRepo.java
+++ src/main/java/dmacc/repository/MovieRepo.java
@@ -5,10 +5,16 @@
  */
 package dmacc.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dmacc.beans.Movie;
 
 public interface MovieRepo extends JpaRepository<Movie, Long>{
-
+	
+	List<Movie> findByReleaseDateBetweenOrderByReleaseDate(LocalDate startDate, LocalDate endDate);
+	
+	List<Movie> findByReleaseDateBeforeOrderByGenre(LocalDate endDate);
 }
